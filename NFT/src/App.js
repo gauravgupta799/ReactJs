65f9646dbb9ac27e98d1a47bb8922caafe014508
Gaurav Gupta <86770283+gauravgupta799@users.noreply.github.com>
2022-03-07 20:39:41 +0530

    NFTReactProject

---
+++ NFT/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import User from './UserType/User';
+// import UserType from './SelectRole/UserType'
+
+const App = () => {
+  return (
+    <div>
+        <User/>
+        {/* <UserType/> */}
+    </div>
+  )
+}
+
+export default App;
---
+++ NFT/src/SelectRole/UserType.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+	Row,
+	Col,
+    LogoCol,
+	ImageWrapper,
+	FrameWrapper,
+	Paragraph,
+	ParagraphText,
+	Form,
+
+} from "./UsertTpe.style";
+// import WordImage from '../images/worded_logo.png'
+
+const UserType = () => {
+	return (
+		<Row>
+			<Col>
+             
+				
+			</Col>
+
+			<Col>
+				<ImageWrapper>
+					<img src='https://i.ibb.co/481XhQ5/daamImg.png ' alt='daamImage' />
+				</ImageWrapper>
+			</Col>
+		</Row>
+	);
+};
+
+export default UserType;
