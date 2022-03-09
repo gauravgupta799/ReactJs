a5e66ad4d280d3a640108a2d6bc8d09d7cda6c9b
Gaurav Gupta <86770283+gauravgupta799@users.noreply.github.com>
2022-03-09 16:38:06 +0530

    aboutPageDesign

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 import Home from "./PortfolioContainer/Home/Home"
-import About from "./PortfolioContainer/AboutMe/About"
+import About from "./PortfolioContainer/AboutMe/About";
+import Resume from './PortfolioContainer/Resume/Resume';
+import Testimonial from "./PortfolioContainer/Testimonial/Testimonial";
+import Contact from "./PortfolioContainer/Contact/Contact";
 
 function App() {
 	return (
 		<div className='App'>
 			<Home/>
-			<About/>
+			<About/><br/><br/><br/>
+			<Resume/>
+		
+			{/* <Testimonial/> */}
+			{/* <Contact/> */}
 		</div>
 	);
 }
